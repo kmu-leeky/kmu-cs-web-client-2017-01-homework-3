12aad798722ea176ab7d71ffae288e8b289d2cb3
Kyungyong Lee <leeky@kookmin.ac.kr>
2017-05-10 08:15:44 +0900

    adding code skeleton for homework 3

---
+++ functions.js
@@ -0,0 +1,18 @@
+function stringToInt(input) {
+}
+
+function maskNumber(input) {
+}
+
+function getAverage(input_array) {
+}
+
+function isMultipleSeven(input) {
+}
+
+function operation(type, a, b) {
+}
+
+
+function triangleMtn(floor) {
+}
